d7283f27ca2ccbe4f6bb6977d9b284054a8dafd1
Tobias Casaux <casauxtobias@gmail.com>
2026-05-26 23:23:50 -0300

    implementar login(Alan) a mapa

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,9 +1,26 @@
-import { SignIn } from "@clerk/clerk-react";
+import AuthHeader from "../components/Navbar/AuthHeader";
+import LoginCard from "../components/Auth/LoginCard";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <SignIn redirectUrl="/mapa" />
+    <div className="relative w-screen h-screen overflow-hidden">
+
+      {/* VIDEO DE FONDO */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src="https://www.villamaria.gob.ar/media/home/video_desktop_1080.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+
+      {/* OVERLAY */}
+      <div className="absolute inset-0 bg-black/30" />
+
+      <AuthHeader />
+      <LoginCard />
+
     </div>
   );
 }
